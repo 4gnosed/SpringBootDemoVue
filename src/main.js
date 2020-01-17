4497beb6b14dfadca6352f3f041dbbac73a4f654
4gnosed <gnosed@foxmail.com>
2020-01-17 17:18:10 +0800

    配合后端基于shiro的拦截器

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import store from './store'
 // 设置反向代理，前端请求默认发送到 http://localhost:8443/api
 var axios = require('axios')
 axios.defaults.baseURL = 'http://localhost:8443/api'
+// 前端每次发送请求时就会带上 sessionId
+axios.defaults.withCredentials = true
 // 全局注册，之后可在其他组件中通过 this.$axios 发送数据
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
@@ -20,8 +22,12 @@ Vue.use(ElementUI)
 // 如果存在，则放行，否则跳转到登录页面，并存储访问的页面路径（以便在登录后跳转到访问页）
 router.beforeEach((to, from, next) => {
   if (to.meta.requireAuth) {
-    if (store.state.user.username) {
-      next()
+    if (store.state.user) {
+      // next()
+      // 访问每个页面前都向后端发送一个请求,后端拦截器验证登录状态
+      axios.get('/authentication').then(resp => {
+        if (resp) next()
+      })
     } else {
       next({
         path: 'login',
